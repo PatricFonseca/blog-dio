99ce13739dba85b6d8b214343e0dad95511adac6
patricfonseca <patric.ccomp@gmail.com>
2023-12-13 00:26:44 -0300

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { BigcardComponent } from './components/bigcard/bigcard.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, BigcardComponent],
+  declarations: [AppComponent, HomeComponent, BigcardComponent, SmallCardComponent],
   providers: [],
   bootstrap: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, MenuComponent],
--- src/app/components/bigcard/bigcard.component.ts
+++ src/app/components/bigcard/bigcard.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bigcard',
   templateUrl: './bigcard.component.html',
-  styleUrls: ['./bigcard.component.scss']
+  styleUrls: ['./bigcard.component.scss'],
 })
 export class BigcardComponent {
+  @Input()
+  title: string = 'The game awards';
+  @Input()
+  imageUrl: string =
+    'https://www.ojilo.com.br/arquivos_personalizados/uploads/noticias/fotos/thegameawards2022capa_rm92_657365cf1f091.jpg';
 
+  @Input()
+  contentText: string =
+    "Aconteceu ontem (7), o The Game Awards com diversos trailers e a premiação em várias categorias. Como era esperado, Baldur's Gate 3 foi o grande vencedor da noite, eleito o Jogo do ano.";
 }
